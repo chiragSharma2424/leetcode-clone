65b8b994973b26525fd37b55aa78b64d778fc7ce
chirag <sharmachirag242004@gmail.com>
2025-09-17 11:23:04 +0530

    user rigister:

--- backend/src/controllers/userAuth-controller.js
+++ backend/src/controllers/userAuth-controller.js
@@ -1,17 +1,44 @@
 import userModel from "../models/user-model.js";
 import { validate } from "../utils/validator.js";
+import jwt from 'jsonwebtoken'
+import dotenv from 'dotenv';
+dotenv.config();
+import bcrypt from 'bcrypt';
 
 async function register(req, res) {
     try {
+        // we have to validate data
+        validate(req.body);
         const { firstName, emailId, password } = req.body;
 
-        // we have to validate the data
-
-        if(!firstName || !emailId || !password) {
+        const existingUser = await userModel.findOne({ emailId });
+        if(existingUser) {
             return res.status(400).json({
-                msg: "All fields are required"
+                msg: "user already exists"
             })
         }
+
+        const hashedPassword = await bcrypt.hash(password, 10);
+
+        // creating user in db
+        const user = await userModel.create({
+            firstName: firstName,
+            emailId: emailId,
+            password: hashedPassword
+        });
+
+        
+        // jwt.sign('payload', 'key', 'time')
+        const token = jwt.sign({ _id: user._id, emailId: emailId }, process.env.JWT_SECRET, { expiresIn: '1h' });
+        // setting token in cookie
+        res.cookie('token', token, {maxAge: 60*60*1000});
+
+
+        return res.status(201).json({
+            msg: "user register successfully",
+            user: user
+        });
+
     } catch(err) {
         console.log(`error in user auth controller ${err}`);
         return res.status(500).json({
--- backend/src/utils/validator.js
+++ backend/src/utils/validator.js
@@ -5,4 +5,17 @@ export const validate = (data) => {
     const isAllowed = mandatoryField.every((k) => {
         Object.keys(data).includes(k);
     });
+
+
+    if(!isAllowed) {
+        throw new Error("Some field missing");
+    }
+
+    if(validator.isEmail(data.emailId)) {
+        throw new Error("Invalid Email");
+    }
+
+    if(validator.isStrongPassword(data.password)) {
+        throw new Error("Weak password");
+    }
 }
