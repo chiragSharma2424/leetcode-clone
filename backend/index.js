61f56eea461b837f8be7e83b2070cb1e9c2c0358
chirag <sharmachirag242004@gmail.com>
2025-09-17 10:49:29 +0530

    user model done

--- backend/index.js
+++ backend/index.js
@@ -1,9 +1,15 @@
 import express from 'express';
+import cookieParser from 'cookie-parser';
+import cors from 'cors';
 import dotenv from 'dotenv';
 import main from './src/config/db.js';
 dotenv.config();
 const app = express();
 const port = process.env.PORT;
+
+app.use(express.json());
+app.use(cors());
+app.use(cookieParser());
 
 app.get('/', (req, res) => {
     return res.status(200).json({
